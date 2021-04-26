8881bf373638fbac3873629d02351a3cb92c4d69
Greg Martin <gregmartinucb@gmail.com>
2021-04-26 00:27:12 -0700

    Added ability to select item from list and update JS with the information for that resource. Currently working on dyamically creating input fields for each ingredient

--- public/clientJS.js
+++ public/clientJS.js
@@ -1,14 +1,40 @@
+var resourceJson =[];
 
 async function getResources(){
     const resourceResponse = await fetch('../resources');
-    const resourceJson = await resourceResponse.json();
+    resourceJson = await resourceResponse.json();
 
     let resourceSelection = document.getElementById('productSelect');
     resourceJson.forEach((resource) => {
         let opt = document.createElement("option");
         opt.innerHTML = resource.name;
+        opt.value = resource.db_letter;
         resourceSelection.appendChild(opt);
     });
 
     console.log(resourceJson);
+}
+
+productID = 0;
+ingredients = [];
+productJson = [];
+
+const sel = document.getElementById('productSelect');
+document.getElementById("selectProduct").onclick = function() {
+    console.log(sel.value);
+    productID = sel.value;
+    resourceJson.forEach((item) => {
+        if(item.db_letter == productID){
+            productJson = item;
+            ingredients = item.ingredients;
+            //console.log(ingredients);
+            //console.log(ingredients.length);
+        }
+    });
+    ingredients.forEach((ingredient, i) => {
+        let ing = document.createElement("input");
+        ing.type = "number";
+    });
+
+
 }
