a1745d338fbfa863cf3cb55587151dce73c8f738
Greg Martin <gregmartinucb@gmail.com>
2021-04-25 21:01:43 -0700

    Initializing the git repo. This is the beginning.

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const DataStore = require('nedb');
+const fetch = require('node-fetch');
+require('dotenv').config();
+const app = express();
+
+app.listen(3000,() => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+const resourceDatabase = new DataStore('resourceDatabase.db');
+resourceDatabase.loadDatabase();
+
+const resourceBaseURL = `https://www.simcompanies.com/api/v3/en/encyclopedia/resources/0/`;
+const imagesAPIURL = `https://d1fxy698ilbz6u.cloudfront.net/static/`;
+
+
+
+
+/*
+This function will update the encyclopedia resource entries. Only needs to be
+updated if the resources base information changes.
+*/
+//currentResource = 1
+//setInterval(function (){
+//    getEncyclopediaData(currentResource)
+//}, 3000);
+
+const getEncyclopediaData = async (resourceNumber) => {
+    const encycResponse = await fetch(resourceBaseURL+resourceNumber.toString());
+    const encycJson = await encycResponse.json();
+    try{
+        const resourceName = await encycJson.name;
+        console.log(resourceName);
+        resourceDatabase.loadDatabase();
+        resourceDatabase.update({name:resourceName},encycJson,{ upsert: true }, (err, num) => {
+
+        });
+        resourceDatabase.persistence.compactDatafile();
+    }
+    catch(err){
+        console.log(err);
+    }
+    currentResource += 1
+    if (currentResource >= 120){
+        currentResource = 1
+    }
+};
+
+/* Removes encyclopedia database entries that are a result of not being able to
+    find the resource. getEncyclopediaData loops through 120 items and there
+    is a gap in there where some resource numbers do not have a page.*/
+/*
+const cleanResourceDatabase = () => {
+    resourceDatabase.loadDatabase();
+    resourceDatabase.remove({message:"Could not find such resource"},{multi:true},
+        function(err,numRemoved){
+                console.log(`Removed ${numRemoved} erroneous entries.`);
+    })
+}
+cleanResourceDatabase();
+*/
